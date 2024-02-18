18a6e8ac51dcedd8545cd75cd976c9cd3370c5f4
Dave O'Brien <github@diaspoir.net>
2024-02-18 21:03:28 +0800

    Added some Comments and updated the .gitignore.
    No need to add tags, and need to document that the script is not running
    eval on arbitrary input.

--- restore_env.sh
+++ restore_env.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+# Script to take the external environment (outside of our
+# tmux bubble), and re-apply it, in case we've changed ssh
+# sessions and X11 Forwarding has stopped workijng.
+#
 
 IFS=$'\n'
 for command in `tmux show-environment -s`
 do
     echo "$command"
+    # The tmux command above returns a list of lines for a script
+    # Otherwise, `eval` on arbiotrary input is not recommended.
     eval $command
 done
